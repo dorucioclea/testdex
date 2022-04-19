f1bae4436f369189a35c934d2ec0e1ef37b38acf
sgonzalez77 <sergiogonzalezrubio77@gmail.com>
2022-04-19 13:23:46 +0200

    first commit smart contract testdex

---
+++ contracts/testdex/wasm/src/lib.rs
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    testdex
+    (
+        addLiquidityEgld
+        addLiquidityToken
+        calculateK
+        claimEarnings
+        claimLiquidityEgld
+        claimLiquidityToken
+        egldToToken
+        feeEgldToken
+        feeTokenEgld
+        getEarnings
+        getFee
+        getInitialK
+        getLiquidityEgld
+        getLiquidityToken
+        priceEgldToken
+        priceEgldTokenNoFee
+        priceTokenEgld
+        priceTokenEgldDenominator
+        priceTokenEgldNoFee
+        priceTokenEgldNumerator
+        status
+        tokenToEgld
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
